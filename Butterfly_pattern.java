aed86aa6e417f75242d2791391195a5ebae228a3
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-01-05 02:58:49 +0530

    add butterfly pattern programe

---
+++ Butterfly_pattern.java
@@ -0,0 +1,25 @@
+package BasicProgrames;
+import java.util.Scanner;
+public class Butterfly_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("give no. of rows :");
+        int x = sc.nextInt();
+        for(int i = 1 ; i <= x ; i++){
+            for (int j = 1 ; j <= i ; j++)System.out.print("* ");
+            for (int k = 1 ; k <= x-i ; k++)System.out.print("  ");
+            for (int k = 1 ; k <= x-i ; k++)System.out.print("  ");
+            for (int l = 1 ; l <= i ; l++)System.out.print("* ");
+            System.out.println();
+            }
+        for (int i = x-1 ; i > 0 ; i-- ){
+            for (int j = 1 ; j <= i ; j++ )System.out.print("* ");
+            for (int k = 1 ; k <= x-i ; k++)System.out.print("  ");
+            for (int k = 1 ; k <= x-i ; k++)System.out.print("  ");
+            for (int l = 1 ; l <= i ; l++)System.out.print("* ");
+            System.out.println();
+        }
+         sc.close();
+    }
+}
+
